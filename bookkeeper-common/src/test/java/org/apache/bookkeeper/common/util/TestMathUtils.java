824a66b575ba1679241398538ddd79b77e98417c
wronek97 <bianchidavide97@gmail.com>
2023-12-12 15:42:55 +0100

    TestMathUtils Updates

--- bookkeeper-common/src/test/java/org/apache/bookkeeper/common/util/TestMathUtils.java
+++ bookkeeper-common/src/test/java/org/apache/bookkeeper/common/util/TestMathUtils.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
