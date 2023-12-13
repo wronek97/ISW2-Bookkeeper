e2bc516851abdbb5abb242ed3d4797105fa20177
wronek97 <bianchidavide97@gmail.com>
2023-12-13 17:45:04 +0100

    try badua

--- dfc/report.sh
+++ dfc/report.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
-SER="coverage.ser"
-CLASSES="target/classes"
-XML="target/badua.xml"
+SER="/coverage.ser"
+CLASSES="/target/classes"
+XML="/target/badua.xml"
 
-java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
+#java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
+java -jar ${CLI} report -show-classes -show-methods -xml
