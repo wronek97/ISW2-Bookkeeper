17e22655729315e10dde68a835688b926b2847a2
wronek97 <bianchidavide97@gmail.com>
2023-12-13 18:01:51 +0100

    ba-dua report try

--- dfc/report.sh
+++ dfc/report.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
-SER="./bookkeeper-server/coverage.ser"
-CLASSES="./bookkeeper-server/target/classes"
-XML="./bookkeeper-server/target/badua.xml"
+SER="../bookkeeper-server/coverage.ser"
+CLASSES="../bookkeeper-server/target/classes"
+XML="../bookkeeper-server/target/badua.xml"
 
 #java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
 java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
