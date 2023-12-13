2c6027912b99a16b365ebcd5756c160991c07c22
wronek97 <bianchidavide97@gmail.com>
2023-12-13 17:56:56 +0100

    tanto non va

--- dfc/report.sh
+++ dfc/report.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
-SER="/coverage.ser"
-CLASSES="/target/classes"
-XML="/target/badua.xml"
+SER="./bookkeeper-server/coverage.ser"
+CLASSES="./bookkeeper-server/target/classes"
+XML="./bookkeeper-server/target/badua.xml"
 
 #java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
-java -jar ${CLI} report -show-classes -show-methods -xml
+java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
