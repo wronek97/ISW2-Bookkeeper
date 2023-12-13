56086d6dd2433bf8daca50cecb6d778e48e70a9e
wronek97 <bianchidavide97@gmail.com>
2023-12-13 18:39:27 +0100

    add profile

--- dfc/report.sh
+++ dfc/report.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
-SER="../dfc/coverage.ser"
-CLASSES="../bookkeeper-server/target/classes"
-XML="../bookkeeper-server/target/badua.xml"
+SER="./coverage.ser"
+CLASSES="./target/classes"
+XML="./target/badua.xml"
 
-#java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
-java -jar ${CLI} report -classes ${CLASSES}
+java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
