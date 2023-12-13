ea7025a3addc614758ada8740cadfbddd9132832
wronek97 <bianchidavide97@gmail.com>
2023-12-13 20:50:28 +0100

    Final config commit

--- dfc/report.sh
+++ dfc/report.sh
@@ -2,7 +2,7 @@
 
 CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
 SER="./coverage.ser"
-CLASSES="./bookkeeper-server/target/classes"
+CLASSES="./target/classes"
 XML="./target/badua.xml"
 
 java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
