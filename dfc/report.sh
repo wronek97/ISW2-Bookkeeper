3d2de1370f044714e990db4cbfe5ef37477a88ce
wronek97 <bianchidavide97@gmail.com>
2023-12-13 18:30:08 +0100

    change all

--- dfc/report.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CLI="../dfc/ba-dua-cli-0.6.0-all.jar"
-SER="../dfc/coverage.ser"
-CLASSES="../bookkeeper-server/target/classes"
-XML="../bookkeeper-server/target/badua.xml"
-
-#java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
-java -jar ${CLI} report -classes ${CLASSES}
